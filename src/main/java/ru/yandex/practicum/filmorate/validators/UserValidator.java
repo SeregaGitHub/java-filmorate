3f5636acf85b8ce09a58bf4d0057876df7e9f9ec
Sergei Kraev <serega.kraev1@yandex.ru>
2023-02-15 13:14:36 +0300

    refactor: add new feature in controllers, exceptions, ErrorHandler

--- src/main/java/ru/yandex/practicum/filmorate/validators/UserValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validators/UserValidator.java
@@ -1,17 +1,19 @@
 package ru.yandex.practicum.filmorate.validators;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 
+@Component
 @Slf4j
 public class UserValidator {
 
-    public static User userNameValidation(User user) {
+    public User userNameValidation(User user) {
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
+            log.info("Ввиду отсутствия у пользователя с логином " + user.getLogin()
+                    + " имени, его имя будет идентично его логину.");
         }
-        log.info("Ввиду отсутствия у пользователя с логином " + user.getLogin()
-                + " имени, его имя будет идентично его логину.");
         return user;
     }
 }
